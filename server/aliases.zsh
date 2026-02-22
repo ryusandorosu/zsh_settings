975550b95f08ef7594aa0118d35937e6b43599df
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-02-22 20:33:32 +0300

    added an alias to watch fail2ban logs

--- server/aliases.zsh
+++ server/aliases.zsh
@@ -1,3 +1,4 @@
 alias unbanip='sudo fail2ban-client set sshd unbanip'
 alias banip='sudo fail2ban-client set sshd banip'
 alias banlist='sudo fail2ban-client status sshd'
+alias banlog='sudo cat /var/log/fail2ban.log'
