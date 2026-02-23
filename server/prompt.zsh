3278a97c7c5104ef94582dbb5c0b29ded3be0b00
Bulat Sibgatullin <ryusandorosu@DESKTOP-5EFI5KM>
2026-02-23 14:32:04 +0300

    modified condition of prompt redefining, depending on theme

--- server/prompt.zsh
+++ server/prompt.zsh
@@ -1,5 +1,7 @@
 custom_hostname='nuc-server'
 #%m for short hostname, %M for full hostname
+if [[ "$ZSH_THEME" == "pmcgee" ]]; then
 PROMPT='%{$fg[$NCOLOR]%}%B%n@$custom_hostname%b%{$reset_color%} %{$fg[white]%}%B${PWD/#$HOME/~}%b%{$reset_color%}
 $(git_prompt_info)%(!.#.$) '
 RPROMPT='[%*]'
+fi
