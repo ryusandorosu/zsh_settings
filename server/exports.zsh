ae7c8fb445bb1e810773d9d69cfb663ab7a0dd46
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-03 02:06:37 +0300

    added pipx export path

--- server/exports.zsh
+++ server/exports.zsh
@@ -1,2 +1,4 @@
 #Needed for Debian server to see command that require sudo and to zsh to highlight them correctly, without this they are painted red
 export PATH="$PATH:/usr/sbin:/sbin"
+# Created by `pipx` on 2026-03-02 18:33:34
+export PATH="$PATH:/home/ryusandorosu/.local/bin"
