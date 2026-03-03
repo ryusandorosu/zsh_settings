652deea0b374f3f544b3e0e8510b55bdcc866124
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-03 05:38:01 +0300

    changed prompt appearance of pmcgee theme for server

--- server/prompt.zsh
+++ server/prompt.zsh
@@ -1,7 +1,7 @@
-custom_hostname='nuc-server'
+# custom_hostname='nuc-server'
 #%m for short hostname, %M for full hostname
 if [[ "$ZSH_THEME" == "pmcgee" ]]; then
-PROMPT='%{$fg[$NCOLOR]%}%B%n@$custom_hostname%b%{$reset_color%} %{$fg[white]%}%B${PWD/#$HOME/~}%b%{$reset_color%}
+PROMPT='%{$fg[blue]%}%B%n@%m%b%{$reset_color%} %{$fg[white]%}%B${PWD/#$HOME/~}%b%{$reset_color%}
 $(git_prompt_info)%(!.#.$) '
 RPROMPT='[%*]'
 fi
