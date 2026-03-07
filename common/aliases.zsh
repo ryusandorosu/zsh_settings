d856a15eff0b4f158f2a9cfa0688650fc3e787be
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-07 06:33:27 +0300

    added du alias and fixed f2b aliases

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -1,1 +1,3 @@
 alias digs='dig +short'
+alias listen='sudo ss -tulpn | grep'
+alias duh='du -ah $1 | sort -rh | head -20'
--- server/aliases.zsh
+++ server/aliases.zsh
@@ -1,11 +1,11 @@
 alias etcgit='sudo git -C /etc'
 alias bat='batcat'
 
-if [[ $0 == unbanip && -z $1 ]]; then F2BJAIL=sshd; else F2BJAIL=$1; fi
+if [[ $0 == unbanip ]]; then F2BJAIL=$1; fi
 alias unbanip='sudo fail2ban-client set $1 unbanip'
-if [[ $0 == banip && -z $1 ]]; then F2BJAIL=sshd; else F2BJAIL=$1; fi
+if [[ $0 == banip ]]; then F2BJAIL=$1; fi
 alias banip='sudo fail2ban-client set $1 banip'
-if [[ $0 == banlist && -z $1 ]]; then F2BJAIL=sshd; else F2BJAIL=$1; fi
+if [[ $0 == banlist ]]; then F2BJAIL=$1; fi
 alias banlist='sudo fail2ban-client status $1'
 
 alias banlog='sudo cat /var/log/fail2ban.log'
