629a4cbeac7951b19c2d9b8036fe149f73733a58
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-08 22:33:39 +0300

    added ansible deploy command alias and made subcategories of aliases

--- server/aliases.zsh
+++ server/aliases.zsh
@@ -1,12 +1,18 @@
+# common
 alias etcgit='sudo git -C /etc'
 alias bat='batcat'
 
-alias unbanip='sudo fail2ban-client set $1 unbanip'
-alias banip='sudo fail2ban-client set $1 banip'
+# ansible
+alias deploytag='sh /home/ryusandorosu/zsh_settings/commands/deploytag'
+
+# fail2ban
 alias banlist='sudo fail2ban-client status $1'
+alias banip='sudo fail2ban-client set $1 banip'
+alias unbanip='sudo fail2ban-client set $1 unbanip'
 
 alias banlog='sudo cat /var/log/fail2ban.log'
 alias authlog='sudo cat /var/log/auth.log'
 
-alias nftbanlist='sudo nft list set inet filter $1'
+# nftables
 alias nftrules='sudo nft list ruleset'
+alias nftbanlist='sudo nft list set inet filter $1'
