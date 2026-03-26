e5c4c5d3cc509c887b601242e68f728a2eddccf9
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-03-26 18:10:56 +0300

    fail2ban alias lacked second argument

--- server/aliases.zsh
+++ server/aliases.zsh
@@ -7,8 +7,8 @@ alias deploytag='sh /home/ryusandorosu/zsh_settings/commands/deploytag'
 
 # fail2ban
 alias banlist='sudo fail2ban-client status $1'
-alias banip='sudo fail2ban-client set $1 banip'
-alias unbanip='sudo fail2ban-client set $1 unbanip'
+alias banip='sudo fail2ban-client set $1 banip $2'
+alias unbanip='sudo fail2ban-client set $1 unbanip $2'
 
 alias banlog='sudo cat /var/log/fail2ban.log'
 alias authlog='sudo cat /var/log/auth.log'
