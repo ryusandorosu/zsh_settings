0473d47022bd15ee89183257794d084842d3fce7
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-04-30 01:01:51 +0300

    changed prompt appearance, made it single lined

--- common/prompt.zsh
+++ common/prompt.zsh
@@ -1,19 +1,20 @@
 if [[ "$ZSH_THEME" == "pmcgee" ]]; then
-PROMPT='%{$fg[$NAMECOLOR]%}'
-PROMPT+='%n'
-PROMPT+='%{$reset_color%}'
-PROMPT+=' @ '
-PROMPT+='%{$fg[$HOSTCOLOR]%}'
-PROMPT+='%m'
-PROMPT+='%{$reset_color%} '
-PROMPT+='%{$fg[$PATHCOLOR]%}%B'
-PROMPT+='${PWD/#$HOME/~}'
-PROMPT+='%b%{$reset_color%}
-$TIMEBACK'
+PROMPT=''
+PROMPT+='$TIMEBACK'
 PROMPT+='%{$fg[$TIMECOLOR]%}'
 PROMPT+=' %* '
 PROMPT+='%k%{$reset_color%} '
-PROMPT+='$(git_prompt_info)%(!.#.$) '
+
+PROMPT+='%{$fg[$HOSTCOLOR]%}'
+PROMPT+='%m'
+PROMPT+='%{$reset_color%} '
+
+PROMPT+='%{$fg[$PATHCOLOR]%}%B'
+PROMPT+='${PWD/#$HOME/~} '
+PROMPT+='%b%{$reset_color%}'
+
+PROMPT+='$(git_prompt_info)'
+# PROMPT+='%(!.#.$) '
 RPROMPT=
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_no_bold[yellow]%}"
 fi
