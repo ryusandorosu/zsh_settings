d8aac19d1a46723f5f1650208e37271141511796
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-05-01 00:30:31 +0300

    made a decent nft short cli

--- server/aliases.zsh
+++ server/aliases.zsh
@@ -8,9 +8,6 @@ alias deploytag='sh /home/ryusandorosu/zsh_settings/commands/deploytag'
 # fail2ban
 alias banlist='sudo fail2ban-client status $1'
 
-alias banlog='sudo cat /var/log/fail2ban.log'
-alias authlog='sudo cat /var/log/auth.log'
-
-# nftables
-alias nftrules='sudo nft list table inet filter'
-alias nftbanlist='sudo nft list set inet filter $1'
+alias banlog='sudo batcat /var/log/fail2ban.log'
+alias authlog='sudo batcat /var/log/auth.log'
+alias nftlog='sudo batcat /var/log/kern.log | grep nft'
