553e564fc4e87d30769865f7c8a9c04b4311dfb1
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-04-30 03:23:42 +0300

    changed prompt color in wsl to cyan back

--- wsl/prompt.zsh
+++ wsl/prompt.zsh
@@ -5,7 +5,7 @@ PATHCOLOR="white"
 TIMEBACK="%K{blue}"
 TIMECOLOR="black"
 elif [[ "$ZSH_THEME" == "passion" ]]; then
-main_prompt_color=white
-arrow_color=green
+main_prompt_color=cyan
+arrow_color=cyan
 cost_prompt_color=grey
 fi
