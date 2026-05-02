e66fa984a62612231fba47b5a4bf004ba7da0e37
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-05-02 17:51:13 +0300

    highlighting color is a variable

--- common/highlighting.zsh
+++ common/highlighting.zsh
@@ -1,7 +1,8 @@
+highlight_color="#d7af87"
 ZSH_HIGHLIGHT_HIGHLIGHTERS=( main brackets pattern )
-ZSH_HIGHLIGHT_STYLES[command]='fg=#d7af87'
-ZSH_HIGHLIGHT_STYLES[builtin]='fg=#d7af87'
-ZSH_HIGHLIGHT_STYLES[alias]='fg=#d7af87'
+ZSH_HIGHLIGHT_STYLES[command]="fg=$highlight_color"
+ZSH_HIGHLIGHT_STYLES[builtin]="fg=$highlight_color"
+ZSH_HIGHLIGHT_STYLES[alias]="fg=$highlight_color"
 ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=cyan'
 ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=cyan'
 ZSH_HIGHLIGHT_STYLES[path]='fg=white'
