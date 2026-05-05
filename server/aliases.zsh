958b7a3214e30ad90114a9048347d4597614b6d5
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-05-05 23:28:44 +0300

    changed nft logs path

--- server/aliases.zsh
+++ server/aliases.zsh
@@ -6,4 +6,4 @@ alias banlist='sudo fail2ban-client status $1'
 
 alias banlog='sudo batcat /var/log/fail2ban.log'
 alias authlog='sudo batcat /var/log/auth.log'
-alias nftlog='sudo batcat /var/log/kern.log | grep nft'
+alias nftlog='sudo batcat /var/log/nftables.log'
