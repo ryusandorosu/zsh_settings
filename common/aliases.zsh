c2611d67c9ce5eee29637dd6ae4a308c2209e605
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-06-18 22:03:20 +0300

    added some useful aliases for journalctl, ls and git

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -3,3 +3,6 @@ alias listen='sudo ss -tulpn | grep'
 alias duh='du -ah $1 | sort -rh | head -20'
 alias sed-crlf="sed -i 's/\r$//'"
 alias bat='batcat'
+alias lah='ls --color=tty -lah'
+alias journal='sudo journalctl -u $1 | tac | grep --max-count=1 --before-context=10000 --regexp="-- Boot" | tac'
+alias gitc='git -C'
