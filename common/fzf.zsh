9d6d01a6356eee31b9bfe0d39adf145d1c7226ce
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-11 02:47:57 +0300

    also added an alias for nvim to use fzf

--- common/fzf.zsh
+++ common/fzf.zsh
@@ -1,13 +1,17 @@
 alias cdf='cd'
+alias nvf='nvim'
 _fzf_comprun() {
   local command=$1
   shift
   case "$command" in
-    cd)         fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
-    cdf|gitc)   fasd -d | sort -nr | awk '{print $2}' \
-              | fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
-    nvim) fzf --style full --preview 'batcat --color=always {}'       "$@" ;;
-    ssh)  fzf --style full --preview 'ping -c1 {}'                    "$@" ;;
-    *)    fzf --style full --preview 'fzf-preview.sh {}'              "$@" ;;
+    cd)       fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
+    cdf|gitc) fasd -d | sort -nr | awk '{print $2}' | \
+              fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
+    nvim) fzf --style full --preview 'batcat --color=always {}'     "$@" ;;
+    # how to utilize gitcls?
+    nvf)  fasd -f | sort -nr | awk '{print $2}' | \
+          fzf --style full --preview 'batcat --color=always {}'     "$@" ;;
+    ssh|autossh)  fzf --style full --preview 'ping -c1 {}'          "$@" ;;
+    *)            fzf --style full --preview 'fzf-preview.sh {}'    "$@" ;;
   esac
 }
