bb881a00da09c987c8d4412ce10d35d1fd000ce4
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-11 02:12:52 +0300

    added gitc option for fzf to cdf as well

--- common/fzf.zsh
+++ common/fzf.zsh
@@ -3,9 +3,9 @@ _fzf_comprun() {
   local command=$1
   shift
   case "$command" in
-    cd)   fzf --style full --preview 'tree -C {} | head -200'         "$@" ;;
-    cdf)  fasd -d | sort -nr | awk '{print $2}' \
-          | fzf --style full --preview 'tree -C {} | head -200'       "$@" ;;
+    cd)         fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
+    cdf|gitc)   fasd -d | sort -nr | awk '{print $2}' \
+              | fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
     nvim) fzf --style full --preview 'batcat --color=always {}'       "$@" ;;
     ssh)  fzf --style full --preview 'ping -c1 {}'                    "$@" ;;
     *)    fzf --style full --preview 'fzf-preview.sh {}'              "$@" ;;
