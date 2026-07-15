e0e2f938dc940d73a0d45a288d5f697438f3330d
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-16 01:08:16 +0300

    replaced sort -nr piping after fasd with --tac flag for fzf

--- common/fzf.zsh
+++ common/fzf.zsh
@@ -4,14 +4,13 @@ _fzf_comprun() {
   local command=$1
   shift
   case "$command" in
-    cd)       fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
-    cdf|gitc) fasd -d | sort -nr | awk '{print $2}' | \
-              fzf --style full --preview 'tree -C {} | head -200'   "$@" ;;
-    nvim) fzf --style full --preview 'batcat --color=always {}'     "$@" ;;
-    # how to utilize gitcls?
-    nvf)  fasd -f | sort -nr | awk '{print $2}' | \
-          fzf --style full --preview 'batcat --color=always {}'     "$@" ;;
-    ssh|autossh)  fzf --style full --preview 'ping -c1 {}'          "$@" ;;
-    *)            fzf --style full --preview 'fzf-preview.sh {}'    "$@" ;;
+    cd)           fzf --style=full --preview 'tree -C {} | head -200'      "$@" ;;
+    cdf|gitc) fasd -d | awk '{print $2}' | \
+              fzf --tac --style=full --preview 'tree -C {} | head -200'    "$@" ;;
+    nvim)     fzf --style=full --preview 'batcat --color=always {}'        "$@" ;;
+    nvf)      fasd -f | awk '{print $2}' | \
+              fzf --tac --style=full --preview 'batcat --color=always {}'  "$@" ;;
+    ssh|autossh)  fzf --style=full --preview 'ping -c1 {}'                 "$@" ;;
+    *)            fzf --style=full --preview 'fzf-preview.sh {}'           "$@" ;;
   esac
 }
