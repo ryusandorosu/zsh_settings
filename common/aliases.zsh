0b065399facdc57fc66fb1a7de03d4aa8f350290
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-16 07:06:25 +0300

    solved gitcls issue, made gitls a commandlet instead of alias, finally made git vim command

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -4,7 +4,6 @@ alias sed-crlf="sed -i 's/\r$//'"
 alias bat='batcat'
 alias lah='ls -lah'
 alias gitc='git -C'
-alias gitls='git ls-files'
 alias sudo='sudo '
 alias diffs='diff -sy --color'
 
@@ -12,7 +11,6 @@ alias gitgrep='git grep --heading --function-context --line-number'
 #fzf-using aliases:
 alias gitgrepf="git grep . | fzf --style=full --preview 'git grep --heading --function-context --line-number --color {3}'"
 alias gitgrepb="git grep --line-number . | fzf --style=full --preview 'batcat --color=always --style=numbers --highlight-line=\$(echo {1} | cut -d: -f2) \$(echo {1} | cut -d: -f1)'"
-# alias gitgrepb="git grep --line-number . | fzf --style=full --preview 'echo {1} | cut -d: -f1 | xargs -r batcat --color=always --highlight-line=$(echo {1} | cut -d: -f2)'"
 alias gitlog="git log --oneline | fzf --multi --style=full --preview 'git show --color {+1}'"
 
 alias ffind="fd . '/' | fzf --style=full --preview='fzf-preview.sh {}' --bind 'focus:+transform-header:file --brief {}'"
