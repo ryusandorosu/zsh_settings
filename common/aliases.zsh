9bc91caacd2d032ce4ef36ac95d0017a58edfab8
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-16 05:06:27 +0300

    modified gitgrep fzf aliases and added fd/locate fzf aliases filetype detection

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -8,8 +8,11 @@ alias gitls='git ls-files'
 alias sudo='sudo '
 alias diffs='diff -sy --color'
 
+alias gitgrep='git grep --heading --function-context --line-number'
 #fzf-using aliases:
+alias gitgrepf="git grep . | fzf --style=full --preview 'git grep --heading --function-context --line-number --color {3}'"
+alias gitgrepb="git grep --line-number . | fzf --style=full --preview 'echo {1} | cut -d: -f1 | xargs -r batcat --color=always'"
 alias gitlog="git log --oneline | fzf --multi --style=full --preview 'git show --color {+1}'"
-alias gitgrep="git grep . | fzf --preview 'echo {1} | cut -d: -f1 | xargs -r batcat --color=always'"
-alias ffind="fd . '/' | fzf --style=full --preview='fzf-preview.sh {}'"
-alias floc="locate -b . | fzf --style=full --preview='fzf-preview.sh {}'"
+
+alias ffind="fd . '/' | fzf --style=full --preview='fzf-preview.sh {}' --bind 'focus:+transform-header:file --brief {}'"
+alias lfind="locate -b . | fzf --style=full --preview='fzf-preview.sh {}' --bind 'focus:+transform-header:file --brief {}'"
