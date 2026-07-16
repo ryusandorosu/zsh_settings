99962510df72233a17c49cfb2202bc0111c032f3
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-16 05:29:35 +0300

    prettified git grep fzf bat preview by highlighting a matching line and finally solved argument piping getting rid of xargs

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -11,7 +11,8 @@ alias diffs='diff -sy --color'
 alias gitgrep='git grep --heading --function-context --line-number'
 #fzf-using aliases:
 alias gitgrepf="git grep . | fzf --style=full --preview 'git grep --heading --function-context --line-number --color {3}'"
-alias gitgrepb="git grep --line-number . | fzf --style=full --preview 'echo {1} | cut -d: -f1 | xargs -r batcat --color=always'"
+alias gitgrepb="git grep --line-number . | fzf --style=full --preview 'batcat --color=always --style=numbers --highlight-line=\$(echo {1} | cut -d: -f2) \$(echo {1} | cut -d: -f1)'"
+# alias gitgrepb="git grep --line-number . | fzf --style=full --preview 'echo {1} | cut -d: -f1 | xargs -r batcat --color=always --highlight-line=$(echo {1} | cut -d: -f2)'"
 alias gitlog="git log --oneline | fzf --multi --style=full --preview 'git show --color {+1}'"
 
 alias ffind="fd . '/' | fzf --style=full --preview='fzf-preview.sh {}' --bind 'focus:+transform-header:file --brief {}'"
