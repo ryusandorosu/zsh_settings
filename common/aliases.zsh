624df8ce39a3fd435055b42a20aa5530e85451a3
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-19 00:18:57 +0300

    enabled search in hidden files and directories by default for ripgrep alias

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -4,7 +4,7 @@ alias sed-crlf="sed -i 's/\r$//'"
 alias sudo='sudo '
 alias diffs='diff -sy --color'
 alias rgrep="rgrep --color=auto --line-number --exclude-dir='.*'"
-alias rg='rg --vimgrep'
+alias rg='rg --hidden --vimgrep'
 
 alias gitc='git -C'
 alias gitgrep='git grep --heading --line-number --before-context=2 --after-context=1' # --function-context: is exclusive with --*context flags
