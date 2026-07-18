8fa8c3c91138d120ef3dd081df89435ae07192fd
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-18 06:31:24 +0300

    deleted empty commented completion lah and batcat files after replacement their functions with common pwd function

--- common/fzf/completion/aliases.zsh
+++
@@ -1,8 +0,0 @@
-# _fzf_complete_lah() {
-#   local base=$(_fzf_base_dir "$prefix")
-#   if [[ $base == . ]]; then
-#     _fzf_complete -- "$@" < <(fd --strip-cwd-prefix=always --type d .)
-#   else
-#     _fzf_complete -- "$@" < <(fd --type d . "$base")
-#   fi
-# }
