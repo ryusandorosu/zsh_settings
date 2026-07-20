6faa023ebae33a87d0925deec7349a5914e53f0b
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-20 19:05:32 +0300

    added reverse alias search using fzf

--- common/fzf/completion/alias.zsh
+++ common/fzf/completion/alias.zsh
@@ -2,7 +2,6 @@ _fzf_complete_alias() {
   _fzf_complete --prompt="alias> " \
     --delimiter='\t' \
     --with-nth=1 \
-    --preview='printf "%s\n" {2}' \
     -- "$@" < <(
       for k in "${(@k)aliases}"; do
         print -r -- "${k}"$'\t'"${aliases[$k]}"
@@ -14,3 +13,17 @@ _fzf_complete_alias_post()   { cut -f1; }
 
 _fzf_complete_unalias()      { _fzf_complete_alias "$@"; }
 _fzf_complete_unalias_post() { _fzf_complete_alias_post; }
+
+_fzf_complete_ralias() {
+  _fzf_complete --prompt="reverse alias> " \
+    --delimiter='\t' \
+    --with-nth=1 \
+    -- "$@" < <(
+      for k in "${(@k)aliases}"; do
+        [[ $k == "" ]] && continue
+        print -r -- "${aliases[$k]}"$'\t'"${k}"
+      done | grep -vP '^\s+|^$' | sort
+    )
+}
+alias ralias='alias'
+_fzf_complete_ralias_post() { cut -f2; }
--- common/fzf/comprun.zsh
+++ common/fzf/comprun.zsh
@@ -4,7 +4,7 @@ _fzf_comprun() {
   local command=$1
   shift
   case "$command" in
-    cd|lah|l|ls|lsa)          preview_tree "{}"; bind_fileinfo "{}"
+    cd|l|ls|lsa|lah)          preview_tree "{}"; bind_fileinfo "{}"
               fzf "${fzstyle[@]}" "${previewcmd[@]}" "${briefinfo[@]}"  "$@" ;;
 
     vim|nvim)                 preview_bat "{}";  bind_fileinfo "{}"
@@ -15,7 +15,7 @@ _fzf_comprun() {
     cp|mv)                 preview_battree "{}"; bind_fileinfo "{}"
               fzf "${fzstyle[@]}" "${previewcmd[@]}" "${briefinfo[@]}"  "$@" ;;
 
-    alias|unalias)  fzf "${fzstyle[@]}" --preview='printf "%s\n" {2}'   "$@" ;;
+    *alias)   fzf "${fzstyle[@]}" --preview='printf "%s\n" {2}'         "$@" ;;
 
     whence)   fzf "${fzstyle[@]}" \
               --preview='print {} | sed -r "s/^\w+\s+//"'               "$@" ;;
