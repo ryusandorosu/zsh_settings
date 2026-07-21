18bd6f42a00607764278f152165d0bac72966cf9
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-21 22:39:42 +0300

    rewrited gitgrep as gitadd to simplify its dependence on variables, got rid of repo_flag var as well

--- fzf/command-functions/gitgrep.zsh
+++ fzf/command-functions/gitgrep.zsh
@@ -5,50 +5,34 @@ source $ZSHREP/fzf/presets/main.sh
 # idk why $repo_path substitution in cmd array and separately as a variable doesnt work properly in these functions unlike in gitadd/gitvim. sort out it later.
 
 gitgrepf() {
-  if [[ -z "$1" ]]; then
-    cmd=(
-      git
-      grep
-      "."
-    )
-  else
-    if   [[ -d "$1" ]]; then repo_flag="-C $1";
-    elif [[ -f "$1" ]]; then repo_flag="-C $(dirname $1)"; fi
-    repo_path="$(git -C $1 rev-parse --show-toplevel)/"
-    cmd=(
-      git
-      -C $1
-      grep
-      "."
-    )
-  fi
+  gitcmd=(git)
+  if   [[ -d "$1" ]]; then gitcmd+=(-C "$1");
+  elif [[ -f "$1" ]]; then gitcmd+=(-C "$(dirname $1)"); fi
+  repo_path="$("${gitcmd[@]}" rev-parse --show-toplevel)/"
+
+  gitcmd+=(grep)
+  cmd=("${gitcmd[@]}")
+  cmd+=(".")
 
   "${cmd[@]}" | \
   fzf "${fzfdefaults[@]}" \
-  --preview 'git '$repo_flag' grep --heading --function-context --line-number --color {3}'
+  --preview ''${(j: :)gitcmd[@]}' --heading --function-context --line-number --color {3}'
    # --function-context: is exclusive with --*context flags
 }
 
 gitgrepb() {
-  if [[ -z "$1" ]]; then
-    cmd=(
-      git
-      grep
-      --line-number
-      "."
-    )
-  else
-    repo_path="$(git -C $1 rev-parse --show-toplevel)/"
-    cmd=(
-      git
-      -C $1
-      grep
-      --line-number
-      "."
-    )
-  fi
+  gitcmd=(git)
+  if   [[ -d "$1" ]]; then gitcmd+=(-C "$1");
+  elif [[ -f "$1" ]]; then gitcmd+=(-C "$(dirname $1)"); fi
+  repo_path="$("${gitcmd[@]}" rev-parse --show-toplevel)/"
 
-  "${cmd[@]}" | \
+  gitcmd+=(
+    grep
+    --line-number
+    "."
+  )
+
+  "${gitcmd[@]}" | \
   fzf "${fzfdefaults[@]}" \
   --preview 'bat --color=always --style=changes,numbers --highlight-line=$(cut -d: -f2 <<< {1}) '$repo_path'$(cut -d: -f1 <<< {1})'
 }
