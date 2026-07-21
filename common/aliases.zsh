f2d600919077ade47c831764571f3f464f922a0d
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-21 19:12:22 +0300

    added ignoring git subfolder content from ripgrep search results

--- common/aliases.zsh
+++ common/aliases.zsh
@@ -4,7 +4,7 @@ alias sed-crlf="sed -i 's/\r$//'"
 alias sudo='sudo '
 alias diffs='diff -sy --color'
 alias rgrep="rgrep --color=auto --line-number --exclude-dir='.*'"
-alias rg='rg --hidden --vimgrep'
+alias rg="rg --hidden --vimgrep --glob='!.git'"
 alias ripgrep='rg'
 
 alias gitc='git -C'
