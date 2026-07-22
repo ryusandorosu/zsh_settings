2a3075bfdb2cd7ebf38381a5b23924d31852096e
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-22 23:00:17 +0300

    added debugging info for gitpreview while observed passing status=$4 arg to preview_git function before switching to porcelain=v2; added condition for a deleted file

--- fzf/presets/previews/gitpreview.sh
+++ fzf/presets/previews/gitpreview.sh
@@ -21,15 +21,22 @@ preview_git() {
   esac
 
   # to add a case when a file is staged (M ) and differ it from modified but unstaged ( M). when: one of already staged files has been changed (MM), staged ones are not previewed and no bind_gitinfo output as well
-  # to add a case whan a file is deleted. "fatal: ambiguous argument 'fzf/presets/previews/git.sh': unknown revision or path not in the working tree."
+  # 'git diff --cached' for (M ), 'git diff' for ( M) as is
   # to add a case whan a file is renamed (R). preview gets an old name as {2}, while new name is seems to be {3}. (RM) = modified after being staged, a different case
   previewcmd=(
     --preview
     "
     __status='$4'
+    x=\${__status[1]}
+    y=\${__status[2]}
+    print \"debug status: \'\$__status\'; x=\'\$x\' y=\'\$y\'\"
     case \"\$__status\" in
-      '??'|A)   test -f '$path' && bat --color=always --style=changes,numbers '$path' || tree -C '$path' ;;
-      *)                             git $repo_flag $gitcommand --color=always --word-diff=plain $select ;;
+      '??'|*A*)
+        test -f '$path' && \
+        bat --color=always --style=changes,numbers '$path' \
+        || tree -C '$path'                                                   ;;
+      *D*)                                                              exit ;;
+      *) git $repo_flag $gitcommand --color=always --word-diff=plain $select ;;
     esac
     "
     --preview-window
