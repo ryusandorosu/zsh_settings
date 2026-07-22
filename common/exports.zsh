3095c1b97f8cb64c580d5debf10cdf0038907fab
Bulat Sibgatullin <boulate.sibgatoulline@gmail.com>
2026-07-23 00:02:47 +0300

    disabled brew hints on environment settings

--- common/exports.zsh
+++ common/exports.zsh
@@ -5,5 +5,6 @@ export PATH=$PATH:$HOME/homeserver-ansible/tools
 
 if [[ "$OS_ID" != Darwin ]]; then
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv zsh)"
+export HOMEBREW_NO_ENV_HINTS=1 # man brew
 eval "$(fasd --init auto)"
 fi
